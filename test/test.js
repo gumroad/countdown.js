1d6cf13f7fdf71bf3254421b6766acfa575bb5d4
squidarth <sshanker220@gmail.com>
2013-07-09 16:30:14 -0700

    changed to global function

--- test/test.js
+++ test/test.js
@@ -1,61 +1,67 @@
 var assert = require("assert")
-  , $ = require("jquery")
   , sinon = require('sinon')
-  , clock;
+  , clock
+  , tickFunction
+  , callbackFunction;
 
-  global.jQuery = $;
+Countdown = require("../lib/countdown");
 
-require("../lib/jquery.countdown");
-
-describe("jquery.countdown", function() {
+describe("countdown.js", function() {
   beforeEach(function() {
     clock = sinon.useFakeTimers();
+    tickFunction = sinon.spy();
+    callbackFunction = sinon.spy();
   });
 
   afterEach(function() {
     clock.restore();
   });
 
-  describe("$.countdown", function() {
+  describe("Countdown.start", function() {
     it("executes onTick while countdown is running", function() {
-      var spy1 = sinon.spy()
-        , spy2 = sinon.spy();
-
-      $.countdown(5, spy1, spy2);
-      assert(spy1.callCount == 1);
+      new Countdown(5, tickFunction, callbackFunction);
+      assert(tickFunction.callCount == 1);
       clock.tick(1000);
-      assert(spy1.callCount == 2);
+      assert(tickFunction.callCount == 2);
       clock.tick(1000);
-      assert(spy1.callCount == 3);
+      assert(tickFunction.callCount == 3);
       clock.tick(1000);
-      assert(spy1.callCount == 4);
+      assert(tickFunction.callCount == 4);
+      clock.tick(1000)
+      assert(tickFunction.callCount == 5);
+      clock.tick(1000)
+      assert(tickFunction.callCount == 5);
     });
 
-    it("executes callback", function() {
-      var spy1 = sinon.spy()
-        , spy2 = sinon.spy();
-
-      $.countdown(5, spy1, spy2);
+    it("executes onComplete", function() {
+      new Countdown(5, tickFunction, callbackFunction);
+      clock.tick(4000);
+      assert(callbackFunction.callCount == 0);
       clock.tick(5000);
-      assert(spy2.callCount == 1);
+      assert(callbackFunction.callCount == 1);
     });
   });
-
+  
   describe("aborting countdown", function() {
+    var countdown;
+
+    beforeEach(function() {
+      countdown = new Countdown(5, tickFunction, callbackFunction);
+    });
+
     it("calls onTick the correct number of times", function() {
-      var spy1 = sinon.spy()
-        , spy2 = sinon.spy();
-
-      var countdown = $.countdown(5, spy1, spy2);
-
-      assert(spy1.callCount == 1);
+      assert(tickFunction.callCount == 1);
       clock.tick(1000);
-      assert(spy1.callCount == 2);
+      assert(tickFunction.callCount == 2);
       countdown.abort();
       clock.tick(1000);
-      assert(spy1.callCount == 2);
-      clock.tick(3000);
-      assert(spy2.callCount == 0);
+      assert(tickFunction.callCount == 2);
+    });
+    
+    it("does not call onComplete", function() {
+      countdown.abort(); 
+      clock.tick(5000);
+      assert(callbackFunction.callCount == 0);
     });
   });
 });
